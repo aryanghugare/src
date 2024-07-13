6189dab488ec377b5a8d805a0947b102b538ab5a
Apple <apple@Apples-MacBook-Pro.local>
2024-07-14 01:28:54 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,147 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // write your code
+       // System.out.println("The sum is");
+       //
+        //int num1 = 6;
+       // int num2 = 4;
+      //  int num3 = 4;
+       // byte age = 34;
+         //System.out.println(age);
+       // Scanner sc = new Scanner(System.in);
+      //  System.out.println("Enter the first number");
+       // float a = sc.nextFloat();
+      //  System.out.println("Enter the second number");
+      //
+        // float b = sc.nextFloat();
+      //  float sum = a+b;
+      //  System.out.println(sum);
+// program to convert marks as input given by the students
+
+
+
+ // Scanner student = new Scanner(System.in);
+
+       // System.out.println("The marks of five subjects");
+
+      //  System.out.println("the marks of the first subject ");
+      //  float a = student.nextFloat();
+
+       // System.out.println("the marks of the second  subject ");
+        // float b = student.nextFloat();
+
+      //  System.out.println("the marks of the third subject ");
+     //   float c = student.nextFloat();
+
+
+      //  System.out.println("the marks of the fourth subject ");
+      //  float d  = student.nextFloat();
+
+      //  System.out.println("the marks of the fifth subject ");
+      //  float e = student.nextFloat();
+
+
+     //   float sum = a + b + c + d + e ;
+       // float per = sum / 500 *100 ;
+
+      //  System.out.println(per);
+
+
+      /*  Scanner User = new Scanner(System.in);
+           System.out.println("Enter your name ");
+         String name  = User.next();
+           System.out.println("have a good day" + name + "Buddy"); */
+
+
+      /*  System.out.println("Enter the marks of the five  Subjects");
+        Scanner sc =  new Scanner(System.in) ;
+
+        System.out.println("Enter the marks of the first subject");
+        float a = sc.nextFloat();
+
+        System.out.println("Enter the marks of the second subject");
+        float b = sc.nextFloat();
+
+        System.out.println("Enter the marks of the third subject");
+        float c = sc.nextFloat();
+
+        System.out.println("Enter the marks of the fourth subject");
+        float d = sc.nextFloat();
+
+        System.out.println("Enter the marks of the fifth subject");
+        float e = sc.nextFloat();
+
+        float f = (a + b + c + d + e) / 500 * 100 ;
+
+
+        System.out.println(f);
+
+       */
+
+        // use Comparison operators to find out whether a given number is greater than the user entered number
+
+/*
+        Scanner user = new Scanner(System.in) ;
+        System.out.println("Enter any number");
+        float a = user.nextFloat();
+        System.out.println(a>10);
+
+
+ */
+
+
+      //double num1 = 45.4 ;
+     // double num2 = 34.3 ;
+      //double sum = num1 + num2  ;
+       // System.out.println(sum);
+String aryan = "ghugare" ;
+        System.out.println(aryan);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Main2.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main2 {
+
+
+    public static void main(String[] args) {
+       /* System.out.println("Taking Input from the user");
+        Scanner aryan = new Scanner(System.in);
+        System.out.println("Enter the First Number ");
+        int ar = aryan.nextInt() ;
+        Scanner Ghugare = new Scanner(System.in);
+        System.out.println("Enter the Second Number ");
+        int ghu = Ghugare.nextInt();
+       int  sum = ar + ghu ;
+        System.out.println("The Sum is:");
+        System.out.println(sum);
+*/
+    /*    System.out.println("Percentage Calculator");
+
+        Scanner ar = new Scanner(System.in);
+        System.out.println("Total Marks:");
+        float tot = ar.nextFloat();
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println(" First Subject : ");
+        float a = sc1.nextFloat();
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println(" Second Subject : ");
+        float b = sc2.nextFloat();
+
+        Scanner sc3 = new Scanner(System.in);
+        System.out.println(" Third Subject : ");
+        float c = sc3.nextFloat();
+
+        float sum = ((a+b+c)/tot)*100 ;
+
+
+        System.out.println("Percentage is : ");
+        System.out.println(sum);
+
+*/
+
+     /*   System.out.println("Hii Welcome to vcet ");
+        Scanner hi = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String a = hi.next() ;
+        System.out.println("Hello " + a + " fuck off ");
+
+*/
+
+
+        System.out.println("Conversion of kilometers to miles ");
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Enter the kilometers");
+        Double a = sc.nextDouble();
+        Double b = a*0.621371 ;
+        System.out.println("The number of miles are : ");
+        System.out.println(b);
+
+
+    }
+}
